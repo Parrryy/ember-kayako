a597f48e1e4064870fe4f8235b7f603fbd494ed8
Richard Livsey <richard@livsey.org>
2017-05-18 17:01:36 +0100

    Update to use new messenger

--- index.js
+++ index.js
@@ -7,46 +7,59 @@ module.exports = {
   isDevelopingAddon: function() { return true; },
 
   contentFor: function(type, env) {
-    var config = env.kayako || {};
+    if (type !== 'body-footer') { return; }
+
+    const config = env.kayako || {};
 
     if (!config.subdomain) {
       throw new Error('Kayako config requires subdomain');
     }
 
-    var subdomain      = config.subdomain;
-    var apiURL         = `https://${subdomain}.kayako.com/api/v1`;
-    var cdnURL         = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor`;
-    var jsURL          = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor/javascript/production.min.js`;
-    var themeAvatar    = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor/images/kayaker.png`;
-    var defaultName    = config.defaultName || subdomain;
-    var themeColor     = config.themeColor || "#3198B6";
-    var welcomeMessage = config.welcomeMessage || `How can we help? We’d love to know what brought you to ${defaultName}`;
-
-    if (type === 'body-footer') {
-      return `
-        <script type="text/javascript">
-          ! function(a, e) {
-              window.kayako = e, e.readyQueue = [], e._config = {
-                  apiUrl: "${apiURL}",
-                  cdnUrl: "${cdnURL}",
-                  defaultName: "${defaultName}",
-                  themeColor: "${themeColor}",
-                  themeAvatar: "${themeAvatar}",
-                  welcomeMessage: "${welcomeMessage}"
-              }, e.ready = function(a) {
-                  e.readyQueue.push(a)
-              };
-              var t = a.createElement("div");
-              t.id = "Kykw__app", t["class"] = "k-widget", a.body.appendChild(t);
-              var o = a.createElement("script");
-              o.type = "text/javascript", o.async = !0;
-              var i = "${jsURL}";
-              o.src = i;
-              var s = a.getElementsByTagName("script");
-              s = s[s.length - 1], s.parentNode.insertBefore(o, s)
-          }(document, window.kayako || []);
-        </script>
-      `;
+    if (!config.teamName) {
+      throw new Error('Kayako config requires teamName');
     }
+
+    const subdomain    = config.subdomain;
+    const teamName     = config.teamName;
+    const homeTitle    = config.homeTitle    || "Hello! 👋";
+    const homeSubtitle = config.homeSubtitle || `Welcome to ${teamName}. Let's chat — start a new conversation below.`;
+
+    const widgets = config.widgets || {
+      presence: {
+        enabled: false
+      },
+      twitter: {
+        enabled: false,
+        twitterHandle: "18825184"
+      },
+      articles: {
+        enabled: false,
+        sectionId: 3
+      }
+    };
+
+    const styles = Object.assign({
+      primaryColor: "#25B0E9",
+      homeBackground: "-180deg, #86CFEC 0%, #789AEC 100%",
+      homePattern: "https://assets.kayako.com/messenger/pattern-1.svg",
+      homeTextColor: "#2D3138"
+    }, config.styles || {});
+
+    const SETTINGS = {
+      apiUrl: `https://${subdomain}.kayako.com/api/v1`,
+      messengerUrl: `https://${subdomain}.kayakocdn.com/messenger`,
+      realtimeUrl: "wss://kre.kayako.net/socket",
+      teamName,
+      homeTitle,
+      homeSubtitle,
+      widgets,
+      styles
+    };
+
+    return `
+      <script type="text/javascript">
+        !function(a,b){function c(){var b=a.createElement("iframe");return b.id="kayako-messenger-frame",b.style.border="none",b.style.width="100%",b.style.height="100%",b}function d(){var c=a.createElement("script");return c.async=!0,c.type="text/javascript",c.src=b._settings.messengerUrl,c.crossOrigin="anonymous",c}function e(){var b=a.createElement("div");return b.id="kayako-messenger",b.style.position="fixed",b.style.right=0,b.style.bottom=0,b.style.width=0,b.style.height=0,b}window.kayako=b,b.readyQueue=[],b.ready=function(a){b.readyQueue.push(a)},b._settings=${JSON.stringify(SETTINGS)};var f=a.body.getElementsByTagName("script")[0],g=c(),h=e();f.parentNode.insertBefore(h,f),h.appendChild(g,f),g.contentWindow.document.open(),g.contentWindow.document.write("<!DOCTYPE html>"),g.contentWindow.document.write("<html>"),g.contentWindow.document.write("<head></head>"),g.contentWindow.document.write("<body></body>"),g.contentWindow.document.write("</html>"),g.contentWindow.document.body.appendChild(d()),g.contentWindow.document.close()}(document,window.kayako||{});
+      </script>
+    `;
   }
 };
