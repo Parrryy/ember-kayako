bfdd79773863680cb7d8bffe655f414fdd61045d
Chris Parry <parry@learnium.com>
2018-02-07 14:05:38 +0000

    Added new kayako messenger

--- index.js
+++ index.js
@@ -3,49 +3,39 @@
 module.exports = {
   name: 'ember-kayako',
 
-  isDevelopingAddon: function() { return true; },
+  isDevelopingAddon: function () { return true; },
 
-  contentFor: function(type, env) {
+  contentFor: function (type, env) {
     var config = env.kayako || {};
 
     if (!config.subdomain) {
       throw new Error('Kayako config requires subdomain');
     }
 
-    var subdomain      = config.subdomain;
-    var apiURL         = `https://${subdomain}.kayako.com/api/v1`;
-    var cdnURL         = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor`;
-    var jsURL          = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor/javascript/production.min.js`;
-    var themeAvatar    = `https://${subdomain}.kayako.com/__apps/widget/assets/visitor/images/kayaker.png`;
-    var defaultName    = config.defaultName || subdomain;
-    var themeColor     = config.themeColor || "#3198B6";
-    var welcomeMessage = config.welcomeMessage || `How can we help? We’d love to know what brought you to ${defaultName}`;
+    var subdomain = config.subdomain;
 
     if (type === 'body-footer') {
       return `
-        <script type="text/javascript">
-          ! function(a, e) {
-              window.kayako = e, e.readyQueue = [], e._config = {
-                  apiUrl: "${apiURL}",
-                  cdnUrl: "${cdnURL}",
-                  defaultName: "${defaultName}",
-                  themeColor: "${themeColor}",
-                  themeAvatar: "${themeAvatar}",
-                  welcomeMessage: "${welcomeMessage}"
-              }, e.ready = function(a) {
-                  e.readyQueue.push(a)
-              };
-              var t = a.createElement("div");
-              t.id = "Kykw__app", t["class"] = "k-widget", a.body.appendChild(t);
-              var o = a.createElement("script");
-              o.type = "text/javascript", o.async = !0;
-              var i = "${jsURL}";
-              o.src = i;
-              var s = a.getElementsByTagName("script");
-              s = s[s.length - 1], s.parentNode.insertBefore(o, s)
-          }(document, window.kayako || []);
-        </script>
-      `;
+        <script>
+        (function (d, a) {
+          function c() {
+            var b = d.createElement("script");
+            b.async = !0;
+            b.type = "text/javascript";
+            b.src = a._settings.messengerUrl;
+            b.crossOrigin = "anonymous";
+            var c = d.getElementsByTagName("script")[0];
+            c.parentNode.insertBefore(b, c)
+          } window.kayako = a;
+          a.readyQueue = [];
+          a.newEmbedCode = !0;
+          a.ready = function (b) { a.readyQueue.push(b) };
+          a._settings = { 
+            apiUrl: "https://${subdomain}.kayako.com/api/v1",
+            messengerUrl: "https://${subdomain}.kayakocdn.com/messenger",
+            realtimeUrl: "wss://kre.kayako.net/socket" };
+            window.attachEvent ? window.attachEvent("onload", c) : window.addEventListener("load", c, !1)
+        })(document, window.kayako || {});</script>`;
     }
   }
-};
+}
